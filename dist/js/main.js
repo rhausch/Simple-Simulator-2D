a515c09151cc84994dd453a081580b9bcc3ae2b3
Rob Hausch <rob.hausch@gmail.com>
2016-01-10 22:41:24 -0800

    Added board resizing

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,82 @@
+// Main file. Where the junk is put until I organize it.
+var gameBoard = {
+    height : 40,
+    width: 80,
+    squares: {},
 
-var squares;
-var height = 40;
-var width = 80;
+    getState: function (x, y) {
+        var count = 0;
+
+        count += this.squares[(x + 1 + this.width) % this.width][(y + 1 + this.height) % this.height];
+        count += this.squares[(x + 1 + this.width) % this.width][(y - 0 + this.height) % this.height];
+        count += this.squares[(x + 1 + this.width) % this.width][(y - 1 + this.height) % this.height];
+
+        count += this.squares[(x - 0 + this.width) % this.width][(y + 1 + this.height) % this.height];
+        count += this.squares[(x - 0 + this.width) % this.width][(y - 1 + this.height) % this.height];
+
+        count += this.squares[(x - 1 + this.width) % this.width][(y + 1 + this.height) % this.height];
+        count += this.squares[(x - 1 + this.width) % this.width][(y - 0 + this.height) % this.height];
+        count += this.squares[(x - 1 + this.width) % this.width][(y - 1 + this.height) % this.height];
+
+        if (count === 3 || (this.squares[x][y] && count === 2)) {
+            return 1;
+        }
+
+        return 0;
+    },
+
+    step : function() {
+        var newBoard = this.getEmptyBoard(this.width, this.height);
+        for (var i = 0; i < this.width; i += 1) {
+            for (var j = 0; j < this.height; j += 1) {
+                newBoard[i][j] = this.getState(i, j);
+            }
+        }
+        this.squares = newBoard;
+    },
+
+    getEmptyBoard : function (sizeX, sizeY) {
+        var newBoard = new Array(sizeX);
+        for (var i = 0; i < sizeX; i += 1) {
+            newBoard[i] = new Array(sizeY);
+            for (var j = 0; j < sizeY; j += 1) {
+                newBoard[i][j] = 0;
+            }
+        }
+        return newBoard;
+    },
+
+    clear : function () {
+        this.squares = this.getEmptyBoard(this.width, this.height);
+    },
+
+    toggle : function (x, y) {
+
+        if (this.squares) {
+            if (this.squares[x][y] === 0) {
+                this.squares[x][y] = 1;
+            } else {
+                this.squares[x][y] = 0;
+            }
+        }
+    },
+
+    resize : function (newWidth, newHeight) {
+        var newBoard = this.getEmptyBoard(newWidth, newHeight);
+        if (this.squares !== {}) {
+            for (var i = 0; i < newWidth && i < this.width; i += 1) {
+                for (var j = 0; j < newHeight && j < this.height; j += 1) {
+                    newBoard[i][j] = this.squares[i][j];
+                }
+            }
+        }
+        this.squares = newBoard;
+        this.width = newWidth;
+        this.height = newHeight;
+    }
+
+};
+
 var size = 20;
 
 var running = 0;
@@ -9,23 +84,17 @@ var running = 0;
 var timer;
 var freq = 100;
 
-window.onload = clearBoard;
-
-function clearBoard() {
-    squares = genBoard();
-    tick();
-};
 
 var fps = {
     startTime : 0,
     frameNumber : 0,
-    getFPS : function(){
+    getFPS : function() {
         this.frameNumber++;
-        var d = new Date().getTime(),
-            currentTime = ( d - this.startTime ) / 1000,
-            result = Math.floor( ( this.frameNumber / currentTime ) );
+        var d = new Date().getTime();
+        var currentTime = (d - this.startTime) / 1000;
+        var result = Math.floor((this.frameNumber / currentTime));
 
-        if( currentTime > 1 ){
+        if (currentTime > 1) {
             this.startTime = new Date().getTime();
             this.frameNumber = 0;
         }
@@ -34,124 +103,103 @@ var fps = {
     }
 };
 
-$("#Start").click(function () {
+$('#Start').click(function () {
     running = !running;
     if (running) {
-        $("#Start").prop('value',"Stop");
+        $('#Start').prop('value', 'Stop');
     } else {
-        $("#Start").prop('value',"Start");
+        $('#Start').prop('value', 'Start');
     }
 });
 
-$("#Rand").click(function() {
-   for (var i = 0; i < 50; i += 1) {
-       var x = Math.floor(Math.random() * width);
-       var y = Math.floor(Math.random() * height);
-       squares[x][y] = 1;
-   }
+$('#Rand').click(function() {
+    for (var i = 0; i < 1000 / size; i += 1) {
+        var x = Math.floor(Math.random() * gameBoard.width);
+        var y = Math.floor(Math.random() * gameBoard.height);
+        gameBoard.squares[x][y] = 1;
+    }
 });
-
-
-function tick() {
-    timer = setTimeout(tick, 1000.0/freq);
-    if (running) {
-        squares = nextBoard(squares);
-    }
-    drawBoard();
-};
-
-function genBoard() {
-    var newBoard = new Array(width);
-    for (var i = 0; i < width; i += 1) {
-        newBoard[i] = new Array(height);
-        for (var j = 0; j < height; j += 1) {
-            newBoard[i][j] = 0;
-        }
-    }
-    return newBoard;
-};
 
 function drawBoard() {
-    var Board = $("#GameBoard");
+    var Board = $('#GameBoard');
     var context = Board[0].getContext('2d');
+    var i;
 
-    context.clearRect(0,0,width*size,height*size);
+    context.clearRect(0, 0, gameBoard.width * size, gameBoard.height * size);
 
     context.strokeStyle = 'grey';
     context.beginPath();
-    for (var i = 0; i <= width; i += 1) {
-        context.moveTo(i * size, 0);
-        context.lineTo(i * size, height * size);
-    }
-    for (var i = 0; i <= height; i += 1) {
-        context.moveTo(0, i * size);
-        context.lineTo(width * size, i * size);
+    if (size > 4) {
+        for (i = 0; i <= gameBoard.width; i += 1) {
+            context.moveTo(i * size, 0);
+            context.lineTo(i * size, gameBoard.height * size);
+        }
+        for (i = 0; i <= gameBoard.height; i += 1) {
+            context.moveTo(0, i * size);
+            context.lineTo(gameBoard.width * size, i * size);
+        }
+    } else {
+        context.moveTo(0, 0);
+        context.lineTo(gameBoard.width * size, 0);
+        context.lineTo(gameBoard.width * size, gameBoard.height * size);
+        context.lineTo(0, gameBoard.height * size);
+        context.closePath();
     }
     context.stroke();
 
     context.fillStyle = 'black';
-    for (var i = 0; i < width; i += 1) {
-        for (var j = 0; j < height; j += 1) {
+    for (i = 0; i < gameBoard.width; i += 1) {
+        for (var j = 0; j < gameBoard.height; j += 1) {
             context.beginPath();
-            if (squares[i][j] == 1) {
+            if (gameBoard.squares[i][j] === 1) {
                 context.fillRect(i * size, j * size, size, size);
             }
         }
     }
 
-    $("#FPSvalue").html(fps.getFPS());
-};
+    $('#FPSvalue').html(fps.getFPS());
+}
 
-function getState(x,y,board) {
-    var count = 0;
+$('#cellSize').change(function () {
+    console.log('Change cell size ' + this.value);
 
-    count += board[(x + 1 + width)%width][(y + 1 + height)%height];
-    count += board[(x + 1 + width)%width][(y - 0 + height)%height];
-    count += board[(x + 1 + width)%width][(y - 1 + height)%height];
+    size = this.value;
+    var width = Math.floor(1500.0 / size);
+    var height = Math.floor(1000.0 / size);
 
-    count += board[(x - 0 + width)%width][(y + 1 + height)%height];
-    count += board[(x - 0 + width)%width][(y - 1 + height)%height];
-
-    count += board[(x - 1 + width)%width][(y + 1 + height)%height];
-    count += board[(x - 1 + width)%width][(y - 0 + height)%height];
-    count += board[(x - 1 + width)%width][(y - 1 + height)%height];
-
-    if (count == 3 || (board[x][y] && count == 2)) {
-        return 1;
-    }
-
-    return 0;
-};
-
-function nextBoard(currentBoard) {
-    var newBoard = genBoard();
-    for (var i = 0; i < width; i += 1) {
-        for (var j = 0; j < height; j += 1) {
-
-            newBoard[i][j] = getState(i,j,currentBoard);
-
-        }
-    }
-
-    return newBoard;
-};
-
-$("#GameBoard").click(function (e) {
-    var x = Math.floor(e.offsetX/size);
-    var y = Math.floor(e.offsetY/size);
-
-    if (squares) {
-        if (squares[x][y] == 0) {
-            squares[x][y] = 1;
-        } else {
-            squares[x][y] = 0;
-        }
-    }
+    console.log('new size = ' + width + ' ' + height);
+    gameBoard.resize(width, height);
 
 });
 
-$("#Step").click(function (e) {
-    squares = nextBoard(squares);
+$('#GameBoard').click(function (e) {
+    var x = Math.floor(e.offsetX / size);
+    var y = Math.floor(e.offsetY / size);
+
+    gameBoard.toggle(x, y);
 });
 
+$('#Step').click(function () {
+    gameBoard.step();
+});
+
+function tick() {
+    timer = setTimeout(tick, 1000.0 / freq);
+    if (running) {
+        gameBoard.step();
+    }
+    drawBoard();
+}
+
+function clearBoard() {
+    var width = Math.floor(1500.0 / size);
+    var height = Math.floor(1000.0 / size);
+
+    console.log('new size = ' + width + ' ' + height);
+    gameBoard.clear();
+    gameBoard.resize(width, height);
+    tick();
+}
+
+window.onload = clearBoard;
 
